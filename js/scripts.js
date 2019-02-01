f84512cfef52d2a5782e50e4636e66f8c25e220b
Isaac Hall <isaacdehall@gmail.com>
2019-02-01 11:02:38 -0800

    finish up first draft of project

--- js/scripts.js
+++ js/scripts.js
@@ -3,28 +3,33 @@ function Pizza(){
   this.size=0;
 }
 
-
-
-
 Pizza.prototype.findCost = function (){
-
-  return this.toppings.length * 2;
-
+  var cost = this.toppings.length * 2;
+  cost *= (parseInt(this.size) + 1);
+  return cost
 }
-
-
 
 $(document).ready(function(){
   $("form").submit(function(event){
     event.preventDefault();
     $("#makePizza").show();
+    $("#pizzaCost").show();
     var myPizza = new Pizza();
+    myPizza.size = $("#size").val()
     $("input:checkbox[name=toppings]:checked").each(function(){
       myPizza.toppings.push($(this).val());
     });
-    console.log(myPizza.findCost());
-    $('#makePizza').text(myPizza.toppings + " pizza coming up!");
+    var cost = myPizza.findCost();
+    if (cost === 0){
+      $("#pizzaCost").hide()
+      $("#hiddenCosts").show()
+    };
+
+    $('#makePizza').append(myPizza.toppings + " pizza coming up!");
+    $('#pizzaCost').append("You're lucky it only costs $" + cost);
     $('#Selections').hide();
     console.log("woah")
+
+
   });
 });
