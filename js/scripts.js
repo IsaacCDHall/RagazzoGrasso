31d5f95afceb39dc2cab116b2d130b1bf078b529
Isaac Hall <isaacdehall@gmail.com>
2019-02-01 14:23:51 -0800

    FIX BROKEN CODE. Cost property has been added in the Pizza constructor and factored into the script

--- js/scripts.js
+++ js/scripts.js
@@ -1,34 +1,39 @@
 function Pizza(){
   this.toppings=[];
   this.size=0;
+  this.cost=0;
 }
 
 Pizza.prototype.findCost = function (){
-  var cost = this.toppings.length * 2;
-  cost *= (parseInt(this.size) + 1);
-  return cost
+  this.cost = this.toppings.length * 2;
+  this.cost *= (parseInt(this.size) + 1);
+  return this.cost
 }
 
 $(document).ready(function(){
+  var myPizza = new Pizza();
+
   $('#Selections2').hide();
   $('#Selections3').hide();
   $("form#Selections1").submit(function(event){
     event.preventDefault();
     $("#makePizza").show();
     $("#pizzaCost").show();
-    var myPizza = new Pizza();
     myPizza.size = $("#size").val()
     $("input:checkbox[name=toppings]:checked").each(function(){
       myPizza.toppings.push($(this).val());
     });
-    var cost = myPizza.findCost();
-    if (cost === 0){
+    myPizza.findCost();
+    if (myPizza.cost === 0){
+       return myPizza.cost = 1000
+       console.log(myPizza.cost)
       $("#pizzaCost").hide()
       $("#hiddenCosts").show()
     };
 
+    console.log(myPizza)
     $('#makePizza').append(myPizza.toppings.join(' and ') + " pizza coming up!");
-    $('#pizzaCost').append("You're lucky it only costs $" + cost);
+    $('#pizzaCost').append("You're lucky it only costs $" + myPizza.cost);
     $('#Selections1').hide();
     $('#Selections2').show();
   });
@@ -48,24 +53,28 @@ $(document).ready(function(){
 
 
 
+
   $("form#Selections3").submit(function(event){
+    var mySecondPizza = new Pizza();
     event.preventDefault();
-    $("#makePizza").hide();
-    $("#pizzaCost").hide();
+    // $("#makePizza").hide();
+    // $("#pizzaCost").hide();
     $("#Selections3").hide();
     $('#results').show();
-    var mySecondPizza = new Pizza();
     mySecondPizza.size = $("#size").val()
     $("input:checkbox[name=toppings]:checked").each(function(){
       mySecondPizza.toppings.push($(this).val());
     });
-    var cost = mySecondPizza.findCost();
-    if (cost === 0){
+     mySecondPizza.findCost();
+     console.log(mySecondPizza);
+    if (mySecondPizza.cost === 0){
+      mySecondPizza.cost += 1000
       $("#pizzaCost").hide()
       $("#hiddenCosts").show()
+      console.log("no toppings")
     };
     $('#makePizza').append(mySecondPizza.toppings.join(' and ') + " pizza coming up!");
-    $('#pizzaCost').append("You're lucky it only costs $" + cost);
+    $('#pizzaCost').append("Altogether, you're lookin' at" + myPizza.cost + mySecondPizza.cost);
     $('#Selections1').hide();
   });
 });
